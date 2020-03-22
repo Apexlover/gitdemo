861b6d9c7c7866d2b82538ace569f9dc7ca7cb35
Apex1 <872878018@qq.com>
2020-03-22 17:11:43 +0800

    update by the second developer

--- fileinfo.py
+++ fileinfo.py
@@ -8,3 +8,9 @@ B=pow(a**2+b**2,0.5)
 D=A/B
 D=("%.3f" % D)
 print(D)
+
+a11,a12,a13=map(int,input().split())
+a21,a22,a23=map(int,input().split())
+a31,a32,a33=map(int,input().split())
+A=a11*a22*a33 + a12*a23*a31 + a13*a21*a32 - a13*a22*a31 - a12*a21*a33 - a11*a23*a32
+print(A)
